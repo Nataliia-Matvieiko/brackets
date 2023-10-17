4411585ede90a6de13a7bf9b309e0bdd9d448fbe
Natali Matvieiko <70928980+Nataliia-Matvieiko@users.noreply.github.com>
2023-10-17 21:24:47 +0200

    init: solve the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function getBracketInfo(bracket, bracketsConfig, openedBrackets) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (bracket === bracketsConfig[i][0]) {
+            if (bracketsConfig[i][0] === bracketsConfig[i][1] && openedBrackets.indexOf(i)>-1){
+                return {index: i, isOpen: false}
+            }
+            return {index: i, isOpen: true}
+        } else if (bracket === bracketsConfig[i][1]) {
+            return {index: i, isOpen: false}
+        }
+    }
+    return undefined;
 }
+
+function check(str, bracketsConfig) {
+    console.log(str, bracketsConfig)
+    const openedBrackets = [];
+    for (let i = 0; i < str.length; i++) {
+        const bracket = str[i];
+        const info = getBracketInfo(bracket, bracketsConfig, openedBrackets);
+        if (info.isOpen) {
+            openedBrackets.push(info.index);
+        } else {
+            if (openedBrackets[openedBrackets.length - 1] === info.index) {
+                openedBrackets.pop();
+            } else {
+                return false;
+            }
+        }
+        console.log(i, bracket, info, openedBrackets);
+    }
+    return openedBrackets.length === 0;
+}
+
+module.exports = check;
